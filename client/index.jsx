e33d9157eb84e2f5db18c14a128229b18f13adfe
ahmad <eng.ahmed.anwar@gmail.com>
2022-11-23 08:08:36 +1300

    use the new ReactDOM syntax for rendering react

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { Auth0Provider } from '@auth0/auth0-react'
@@ -7,20 +7,19 @@ import { Auth0Provider } from '@auth0/auth0-react'
 import App from './App'
 import store from './store'
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <Auth0Provider
-      domain="gardenz.au.auth0.com"
-      clientId="sF7Tf4GqnhENJ7l7gArp5c56ZFZ2WOcL"
-      redirectUri={window.location.origin}
-      audience="https://garden/nz/api"
-    >
-      <Provider store={store}>
-        <Router>
-          <App />
-        </Router>
-      </Provider>
-    </Auth0Provider>,
-    document.getElementById('app')
-  )
-})
+const container = document.getElementById('app')
+const root = createRoot(container)
+root.render(
+  <Auth0Provider
+    domain="gardenz.au.auth0.com"
+    clientId="sF7Tf4GqnhENJ7l7gArp5c56ZFZ2WOcL"
+    redirectUri={window.location.origin}
+    audience="https://garden/nz/api"
+  >
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  </Auth0Provider>
+)
