e96bb8f39875906de146b0064435f5b31fd7dbca
frances <franceslhughes@hotmail.com>
2022-11-24 11:12:46 +1300

    converted some with Alizaa to async functions

--- client/subcomponents/volunteers/VolunteerButton/volunteerButtonHelper.js
+++ client/subcomponents/volunteers/VolunteerButton/volunteerButtonHelper.js
@@ -3,30 +3,60 @@ import { dispatch, getState } from '../../../store'
 import { clearWaiting, setWaiting } from '../../../slices/waiting'
 import { showError } from '../../../slices/error'
 
-export function toggleVolunteerStatus(
+// original below:
+// export function toggleVolunteerStatus(
+//   eventId,
+//   willVolunteer,
+//   setVolunteering,
+//   consume = requestor
+// ) {
+//   const storeState = getState()
+//   const { id, token } = storeState.user
+//   if (!id) {
+//     dispatch(showError('Please register or sign in to volunteer.'))
+//   } else {
+//     dispatch(setWaiting())
+//     const routeMethod = willVolunteer ? 'post' : 'delete'
+//     const userData = { userId: id, eventId }
+
+//     return consume('/volunteers', token, routeMethod, userData)
+//       .then(() => {
+//         setVolunteering(willVolunteer)
+//         dispatch(clearWaiting())
+//         return null
+//       })
+//       .catch((error) => {
+//         dispatch(showError(error.message))
+//         return null
+//       })
+//   }
+// }
+
+/////// changed to async below - to be checked
+
+export async function toggleVolunteerStatus(
   eventId,
   willVolunteer,
   setVolunteering,
   consume = requestor
 ) {
-  const storeState = getState()
-  const { id, token } = storeState.user
-  if (!id) {
-    dispatch(showError('Please register or sign in to volunteer.'))
-  } else {
-    dispatch(setWaiting())
-    const routeMethod = willVolunteer ? 'post' : 'delete'
-    const userData = { userId: id, eventId }
+  try {
+    const storeState = getState()
+    const { id, token } = storeState.user
+    if (!id) {
+      dispatch(showError('Please register or sign in to volunteer.'))
+    } else {
+      dispatch(setWaiting())
+      const routeMethod = willVolunteer ? 'post' : 'delete'
+      const userData = { userId: id, eventId }
 
-    return consume('/volunteers', token, routeMethod, userData)
-      .then(() => {
-        setVolunteering(willVolunteer)
-        dispatch(clearWaiting())
-        return null
-      })
-      .catch((error) => {
-        dispatch(showError(error.message))
-        return null
-      })
+      await consume('/volunteers', token, routeMethod, userData)
+      setVolunteering(willVolunteer)
+      dispatch(clearWaiting())
+      return null
+    }
+  } catch (err) {
+    dispatch(showError(err.message))
+    return null
   }
 }
--- client/subcomponents/volunteers/VolunteerListItem/volunteerListItemHelper.js
+++ client/subcomponents/volunteers/VolunteerListItem/volunteerListItemHelper.js
@@ -3,18 +3,35 @@ import { dispatch, getState } from '../../../store'
 import { setWaiting, clearWaiting } from '../../../slices/waiting'
 import { showError } from '../../../slices/error'
 
-export function toggleAttendance(volunteerData, consume = requestor) {
-  const storeState = getState()
-  const { token } = storeState.user
-  dispatch(setWaiting())
+// export function toggleAttendance(volunteerData, consume = requestor) {
+//   const storeState = getState()
+//   const { token } = storeState.user
+//   dispatch(setWaiting())
 
-  return consume('/volunteers', token, 'patch', volunteerData)
-    .then(() => {
-      dispatch(clearWaiting())
-      return true
-    })
-    .catch((error) => {
-      dispatch(showError(error.message))
-      return false
-    })
+//   return consume('/volunteers', token, 'patch', volunteerData)
+//     .then(() => {
+//       dispatch(clearWaiting())
+//       return true
+//     })
+//     .catch((error) => {
+//       dispatch(showError(error.message))
+//       return false
+//     })
+// }
+
+/////////
+
+export async function toggleAttendance(volunteerData, consume = requestor) {
+  try {
+    const storeState = getState()
+    const { token } = storeState.user
+    dispatch(setWaiting())
+
+    await consume('/volunteers', token, 'patch', volunteerData)
+    dispatch(clearWaiting())
+    return true
+  } catch (error) {
+    dispatch(showError(error.message))
+    return false
+  }
 }
