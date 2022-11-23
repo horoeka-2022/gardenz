55a491e5862d42f14acce4bebac64629549090bf
alizaa <alizaaara@yahoo.co.nz>
2022-11-23 17:15:22 +1300

    function working-jest tested

--- client/views/admin/Events/AddEvent/addEventHelper.js
+++ client/views/admin/Events/AddEvent/addEventHelper.js
@@ -3,44 +3,44 @@ import { setWaiting, clearWaiting } from '../../../../slices/waiting'
 import { showError } from '../../../../slices/error'
 import requestor from '../../../../consume'
 
-export function addEvent(event, navigateTo, consume = requestor) {
-  const storeState = getState()
-  const { gardenId, token } = storeState.user
-  const newEvent = {
-    gardenId,
-    ...event,
-  }
-  dispatch(setWaiting())
-  return consume('/events', token, 'post', newEvent)
-    .then(() => {
-      navigateTo(`/gardens/${gardenId}`)
-      return null
-    })
-    .catch((err) => {
-      dispatch(showError(err.message))
-    })
-    .finally(() => {
-      dispatch(clearWaiting())
-    })
-}
+// export function addEvent(event, navigateTo, consume = requestor) {
+//   const storeState = getState()
+//   const { gardenId, token } = storeState.user
+//   const newEvent = {
+//     gardenId,
+//     ...event,
+//   }
+//   dispatch(setWaiting())
+//   return consume('/events', token, 'post', newEvent)
+//     .then(() => {
+//       navigateTo(`/gardens/${gardenId}`)
+//       return null
+//     })
+//     .catch((err) => {
+//       dispatch(showError(err.message))
+//     })
+//     .finally(() => {
+//       dispatch(clearWaiting())
+//     })
+// }
 
 ////////
 
-// export async function addEvent(event, navigateTo, consume = requestor) {
-//   try {
-//     const storeState = getState()
-//     const { gardenId, token } = storeState.user
-//     const newEvent = {
-//       gardenId,
-//       ...event,
-//     }
-//     dispatch(setWaiting())
-//     await consume('/events', token, 'post', newEvent)
-//     navigateTo(`/gardens/${gardenId}`)
-//     return null
-//   } catch (err) {
-//     dispatch(showError(err.message))
-//   } finally {
-//     dispatch(clearWaiting())
-//   }
-// }
+export async function addEvent(event, navigateTo, consume = requestor) {
+  try {
+    const storeState = getState()
+    const { gardenId, token } = storeState.user
+    const newEvent = {
+      gardenId,
+      ...event,
+    }
+    dispatch(setWaiting())
+    await consume('/events', token, 'post', newEvent)
+    navigateTo(`/gardens/${gardenId}`)
+    return null
+  } catch (err) {
+    dispatch(showError(err.message))
+  } finally {
+    dispatch(clearWaiting())
+  }
+}
