e3745d50462ed66a1d4e0fad6305b0549817da17
rav <rav.ariyanayagam@gmail.com>
2022-11-23 17:04:28 +1300

    Change eventHelper to async/await

--- client/views/admin/Events/Event/eventHelper.js
+++ client/views/admin/Events/Event/eventHelper.js
@@ -3,51 +3,51 @@ import { dispatch } from '../../../../store'
 import { setWaiting, clearWaiting } from '../../../../slices/waiting'
 import { showError } from '../../../../slices/error'
 
-export function getEvent(id, user, consume = requestor) {
+export async function getEvent(eventId, user, consume = requestor) {
   dispatch(setWaiting())
-  return consume(`/events/${id}`)
-    .then((res) => {
-      dispatch(clearWaiting())
-      const {
-        id,
-        date,
-        title,
-        gardenId,
-        gardenName,
-        gardenAddress,
-        volunteersNeeded,
-        volunteers,
-        extraVolunteers,
-        description,
-        lat,
-        lon,
-        status,
-      } = res.body
-      const result = {
-        id,
-        date,
-        title,
-        gardenId,
-        gardenName,
-        gardenAddress,
-        volunteersNeeded,
-        description,
-        lat,
-        lon,
-        status,
-      }
-      return user.isAdmin
-        ? {
-            ...result,
-            volunteers,
-            extraVolunteers,
-          }
-        : {
-            ...result,
-            isVolunteer: volunteers.some((v) => v.userId === user.id),
-          }
-    })
-    .catch((error) => {
-      dispatch(showError(error.message))
-    })
+
+  try {
+    const res = await consume(`/events/${eventId}`)
+    dispatch(clearWaiting())
+    const {
+      id,
+      date,
+      title,
+      gardenId,
+      gardenName,
+      gardenAddress,
+      volunteersNeeded,
+      volunteers,
+      extraVolunteers,
+      description,
+      lat,
+      lon,
+      status,
+    } = res.body
+    const result = {
+      id,
+      date,
+      title,
+      gardenId,
+      gardenName,
+      gardenAddress,
+      volunteersNeeded,
+      description,
+      lat,
+      lon,
+      status,
+    }
+    return user.isAdmin
+      ? {
+          ...result,
+          volunteers,
+          extraVolunteers,
+        }
+      : {
+          ...result,
+          isVolunteer: volunteers.some((v) => v.userId === user.id),
+        }
+  } catch (error) {
+    dispatch(showError(error.message))
+  }
 }
