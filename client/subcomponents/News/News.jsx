f22ec76fd0915b593193202a04e785affd91ae99
daph <daphnejasminesimons@gmail.com>
2022-11-25 11:10:07 +1300

    changed indenting slighty, much better

--- client/subcomponents/News/News.jsx
+++ client/subcomponents/News/News.jsx
@@ -6,8 +6,8 @@ export default function News({ news }) {
 
   return (
     <article className="my-6 mx-4 p-6 rounded-3xl border-3 shadow-xl">
-      <h2 className="font-bold ml-6 mt-6 text-lg">{news.title}</h2>
-      <p className="ml-6 text-zinc-400">{createTime}</p>
+      <h2 className="font-bold mt-6 text-lg">{news.title}</h2>
+      <p className=" text-zinc-400">{createTime}</p>
       <p className="my-6">{news.content}</p>
       <p className="text-zinc-400 object-bottom">
         By {news.firstName} {news.lastName}:
