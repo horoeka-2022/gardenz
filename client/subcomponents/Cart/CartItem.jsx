215e5eedef1d5ae4332bb52d1b6bf14cd91b20fb
anna <annaqiu527@gmail.com>
2022-11-24 16:28:28 +1300

    create Cart component

---
+++ client/subcomponents/Cart/CartItem.jsx
@@ -0,0 +1,42 @@
+// import React from 'react'
+
+// import { deleteFromCart, updateCart } from '../slices/cart'
+
+// function CartItem(props) {
+//   const { name, id, quantity, price } = props.item
+
+//   function update(e) {
+//     const newQuantity = e.target.value
+//     const isValidQuantity = !isNaN(Number(newQuantity))
+//     if (isValidQuantity) {
+//       const updateInfo = { id, newQuantity }
+//       // dispatch(updateCart(updateInfo))
+//     }
+//   }
+
+//   function deleteItem() {
+//     // dispatch(deleteFromCart(id))
+//   }
+
+//   const displayQuantity = quantity === 0 ? '' : quantity
+//   return (
+//     <tr>
+//       <td>{name}</td>
+//       <td>
+//         <input
+//           aria-label="quantity"
+//           className="update-input"
+//           value={displayQuantity}
+//           onChange={update}
+//         />
+//       </td>
+//       <td>
+//         <button aria-label="delete" onClick={deleteItem}>
+//           <span className="fa fa-trash fa-2x" />
+//         </button>
+//       </td>
+//     </tr>
+//   )
+// }
+
+// export default CartItem
---
+++ client/views/user/Gardens/Shop/Cart/Cart.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+import CartItem from '../../../../../subcomponents/Cart/CartItem'
+
+function Cart() {
+  const navigate = useNavigate()
+  const cart = window.localStorage.getItem('order')
+
+  function submitCart() {
+    //pass the chosed orders into the deliever page
+    window.localStorage.getItem('order')
+    {
+      /*change the url link with dynamic id later */
+    }
+    navigate('/gardens/:id/shop/delievery/:id')
+  }
+
+  return cart.length ? (
+    <div className="cart">
+      <table>
+        <thead>
+          <tr>
+            <td role="columnheader">Name</td>
+            <td role="columnheader">Price</td>
+            <td role="columnheader">Quantity</td>
+          </tr>
+        </thead>
+        <tbody>
+          {cart.map((item, id) => {
+            return <CartItem key={id} item={item} />
+          })}
+        </tbody>
+      </table>
+      <p className="actions">
+        <button className="button-primary" onClick={submitCart}>
+          Checkout
+        </button>
+      </p>
+    </div>
+  ) : (
+    <p>
+      Your cart is empty! Start shopping{' '}
+      <Link to="/gardens/:id/shop/">here</Link>
+    </p>
+  )
+}
+
+export default Cart
