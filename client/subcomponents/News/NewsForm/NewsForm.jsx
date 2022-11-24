248d1a8d08a6308920f37cc773534cfbe3465988
alizaa <alizaaara@yahoo.co.nz>
2022-11-25 12:18:42 +1300

    centered content & added padding

--- client/subcomponents/News/NewsForm/NewsForm.jsx
+++ client/subcomponents/News/NewsForm/NewsForm.jsx
@@ -26,48 +26,55 @@ export default function NewsForm(props) {
 
   return (
     <>
-      <section>
-        <h2 className="form-title">{props.action}</h2>
-        <form className="form-content" onSubmit={formik.handleSubmit}>
-          <div className="field">
-            <label htmlFor="title" className="label">
-              News Title
-            </label>
-            <Conditional
-              condition={formik.errors.title && formik.touched.title}
-            >
-              <p className="inputError">{formik.errors.title}</p>
-            </Conditional>
-            <input
-              className="form-box"
-              id="title"
-              name="title"
-              type="text"
-              placeholder="news title"
-              onChange={formik.handleChange}
-              value={formik.values.title}
-            />
+      <section className="py-12 flex-auto text-center items-center justify-center">
+        <h2 className="form-title items-center">{props.action}</h2>
+        <form
+          className="form-content flex-auto text-center items-center justify-center flex-col"
+          onSubmit={formik.handleSubmit}
+        >
+          <div className="field items-center justify-center flex-col">
+            <div>
+              {' '}
+              <label htmlFor="title" className="label">
+                News Title
+              </label>
+              <Conditional
+                condition={formik.errors.title && formik.touched.title}
+              >
+                <p className="inputError">{formik.errors.title}</p>
+              </Conditional>
+              <input
+                className="form-box"
+                id="title"
+                name="title"
+                type="text"
+                placeholder="news title"
+                onChange={formik.handleChange}
+                value={formik.values.title}
+              />
+            </div>
+            <div className="items-center justify-center flex-col">
+              <label htmlFor="content" className="label">
+                Content
+              </label>
+              <Conditional
+                condition={formik.errors.content && formik.touched.content}
+              >
+                <p className="inputError">{formik.errors.content}</p>
+              </Conditional>
 
-            <label htmlFor="content" className="label">
-              Content
-            </label>
-            <Conditional
-              condition={formik.errors.content && formik.touched.content}
-            >
-              <p className="inputError">{formik.errors.content}</p>
-            </Conditional>
-
-            <textarea
-              className="content-box"
-              id="content"
-              name="content"
-              placeholder="news content"
-              onChange={formik.handleChange}
-              value={formik.values.content}
-            />
+              <textarea
+                className="content-box"
+                id="content"
+                name="content"
+                placeholder="news content"
+                onChange={formik.handleChange}
+                value={formik.values.content}
+              />
+            </div>
           </div>
 
-          <div className="button-group">
+          <div className="button-group items-center justify-center flex-col">
             <button className="submit form-box" type="submit">
               Submit
             </button>
@@ -77,3 +84,32 @@ export default function NewsForm(props) {
     </>
   )
 }
+
+//////
+
+// // garden logo image
+// <section class="container lg:flex items-center justify-between mx-auto">
+// <img src="/images/gardenzLogoNew.svg" alt="gardenzlogo" class="w-60">
+// <a href="/">
+// </a>
+
+// <nav class="hidden w-full lg:flex justify-between items-center lg:items-center lg:w-auto">
+
+// {/* Home and logout on top right */}
+//   <div class="lg:flex text-center md:text-align">
+//     <a class="text-white hover:text-green block my-6 py-2 px-6" href="/">Change Garden</a>
+//     <a href="/" class="text-white hover:text-green block my-6 py-2 px-6">Sign out</a>
+//   </div>
+// </nav>
+
+// {/* about us - shop */}
+// <nav>
+//   <div class="lg:flex flex-row text-center md:text-align bg-lightGreen">
+//     <a class="text-white hover:text-green block my-6  py-2 px-6 basis-1/4 " href="/gardens/1/about">Update Cover Photo</a>
+//     <a class="text-white hover:text-green block my-6  py-2 px-6 basis-1/4  " href="/gardens/1/events">Add/Edit Events</a>
+//     <a class="text-orange hover:text-green block my-6 py-2 px-6 basis-1/4 " href="/gardens/1/news" aria-current="page">Post News</a>
+//     <a class="text-white hover:text-green block my-6  py-2 px-6 basis-1/4 " href="/gardens/1/gallery">Update Gallery</a>
+//     <a class="text-white hover:text-green block my-6  py-2 px-6 basis-1/4 " href="/gardens/1/shop">Manage Orders</a>
+//     <a class="text-white hover:text-green block my-6  py-2 px-6 basis-1/4 " href="/gardens/1/shop">Volunteers</a>
+//   </div>
+// </nav>
