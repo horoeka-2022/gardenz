04c6b4fd5d8dec073c7a1dbc59119fc810a0679b
jay <jay.watt8000@gmail.com>
2022-11-24 16:09:37 +1300

    Add lightGallery

--- client/views/user/Gardens/Gallery/Gallery.jsx
+++ client/views/user/Gardens/Gallery/Gallery.jsx
@@ -4,6 +4,21 @@ import { useParams } from 'react-router-dom'
 import GardenHeader from '../../../../subcomponents/gardens/GardenHeader/GardenHeader'
 import useGarden from '../../../../hooks/useGarden'
 
+import LightGallery from 'lightgallery/react'
+
+// import styles
+import 'lightgallery/css/lightgallery.css'
+import 'lightgallery/css/lg-zoom.css'
+import 'lightgallery/css/lg-thumbnail.css'
+
+// If you want you can use SCSS instead of css
+// import 'lightgallery/scss/lightgallery.scss'
+// import 'lightgallery/scss/lg-zoom.scss'
+
+// import plugins if you need
+import lgThumbnail from 'lightgallery/plugins/thumbnail'
+import lgZoom from 'lightgallery/plugins/zoom'
+
 function Gallery() {
   const { id } = useParams()
   const { name, imageHeaderUrl } = useGarden(id)
@@ -12,9 +27,14 @@ function Gallery() {
     <>
       <GardenHeader name={name} url={imageHeaderUrl} />
       <section>
-        <p> hi, this is the gallery</p>
-        <img src="/images/comGardenPlant.png" alt="garden image1" />
-        <img src="/images/comGardenRows.png" alt="garden image2" />
+        <LightGallery speed={500} plugins={[lgThumbnail, lgZoom]}>
+          <a href="/images/comGardenPlant.png">
+            <img src="/images/comGardenPlant.png" alt="garden image1" />
+          </a>
+          <a href="/images/comGardenRows.png">
+            <img src="/images/comGardenRows.png" alt="garden image2" />
+          </a>
+        </LightGallery>
       </section>
     </>
   )
