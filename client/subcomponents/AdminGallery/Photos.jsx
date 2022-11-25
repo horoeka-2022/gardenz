df3e0969047de3f25b7d62254b9f58089af6fed6
richard <dev.richard.so@gmail.com>
2022-11-25 16:24:57 +1300

    created admin gallery

--- client/subcomponents/AdminGallery/Photos.jsx
+++ client/subcomponents/AdminGallery/Photos.jsx
@@ -1,15 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import consume from '../../consume'
 
 function Photos() {
   const { id } = useParams()
   const [photos, setPhotos] = useState([])
+  console.log(id)
 
   useEffect(() => {
-    
-  })
+    const fetchPhotos = async () => {
+      const data = await consume(`/gallery/${id}`, '', 'get', {})
+      console.log(data.body)
+      setPhotos(() => data.body)
+    }
+    fetchPhotos()
+  }, [photos])
 
-  return <h2> i am photo</h2>
+  return (
+    <>
+      {photos.map((photo) => {
+        return (
+          <div key={photo.name} className="m-2 basis-1/7">
+            <img
+              className="h-full w-full"
+              src={photo.url}
+              alt={photo.name}
+            ></img>
+          </div>
+        )
+      })}
+    </>
+  )
 }
 
 export default Photos
--- client/views/admin/Gallery/Gallery.jsx
+++ client/views/admin/Gallery/Gallery.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { AiOutlinePlusCircle } from 'react-icons/ai'
 import Photos from '../../../subcomponents/AdminGallery/Photos'
+
 function Gallery() {
   return (
-    <main className="container lg:flex-col m-28 justify-center items-center ">
-      <section className="container lg:flex items-center text-2xl">
-        <h2 className="font-bold">Edit Gallery</h2>
+    <main className="lg:flex-col m-28 justify-center items-center ">
+      <section className="flex items-center text-2xl mb-8">
+        <h2 className="font-bold ml-28">Edit Gallery</h2>
         <i className="text-orange ml-5 font-black">
           <AiOutlinePlusCircle />
         </i>
       </section>
-      <section className="container lg:flex">
+      <section className="lg:flex flex-wrap justify-center">
         <Photos />
       </section>
     </main>
