0e79fb7deacde348ed2a9dec3993a097a5de7c37
daph <daphnejasminesimons@gmail.com>
2022-11-28 11:05:29 +1300

    complete tests for gallery.js and galleryHelper.js

--- client/views/user/Gardens/Gallery/galleryHelper.test.js
+++ client/views/user/Gardens/Gallery/galleryHelper.test.js
@@ -1,5 +1,5 @@
-import { getGalleryImages } from "./galleryHelper";
-import { setWaiting, clearWaiting } from "../../../../slices/waiting";
+import { getGalleryImages } from './galleryHelper'
+import { setWaiting, clearWaiting } from '../../../../slices/waiting'
 import { dispatch } from '../../../../store'
 
 jest.mock('../../../../store')
@@ -8,7 +8,7 @@ afterEach(() => {
   return jest.resetAllMocks()
 })
 
-describe(' -> GET /gallery/${gardenId} api call success', () => {
+describe('-> GET /gallery/${gardenId} api call success', () => {
   it('dispatches with the correct images by Id', () => {
     function consume(path) {
       expect(path).toMatch('1')
@@ -16,12 +16,24 @@ describe(' -> GET /gallery/${gardenId} api call success', () => {
         body: {
           photos: [
             {
-              name: "image2",
-              url: "https://www.kelmarnagardens.nz/uploads/6/0/1/1/60114025/kelmarna-autumn-festival-2019-2-1_orig.jpg",
+              name: 'image2',
+              url: 'https://www.kelmarnagardens.nz/uploads/6/0/1/1/60114025/kelmarna-autumn-festival-2019-2-1_orig.jpg',
+            },
+            {
+              name: 'image3',
+              url: 'https://www.kelmarnagardens.nz/uploads/6/0/1/1/60114025/kelmarna-autumn-festival-2019-2-1_orig.jpg',
             },
           ],
         },
       })
-    } 
-return  getGalleryImages(1, consume).then((photos))
+    }
+    return getGalleryImages(1, consume).then((photos) => {
+      expect(dispatch).toHaveBeenCalledWith(setWaiting())
+      expect(dispatch).toHaveBeenCalledWith(clearWaiting())
+      expect(photos).toHaveLength(2)
+      expect(photos[0].name).toBe('image2')
+      expect(photos[1].name).toBe('image3')
+      expect(photos[0].url).toContain('kelmarna')
+    })
   })
+})
