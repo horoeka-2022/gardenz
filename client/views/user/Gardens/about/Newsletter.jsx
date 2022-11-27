b25e082658346a34bf66468c2dcc404970e2e17d
billie <billielstaples@gmail.com>
2022-11-28 10:05:20 +1300

    Add style on sign up form for news letter subscription page

--- client/views/user/Gardens/about/Newsletter.jsx
+++ client/views/user/Gardens/about/Newsletter.jsx
@@ -9,10 +9,10 @@ export default function Newsletter() {
   return (
     <>
       <GardenHeader name={name} url={imageHeaderUrl} />
-      <p>hello form would be here</p>
-      <div className="border-solid border-2 border-black mx-auto w-3/4 px-20 py-10">
+
+      <div className="shadow-lg shadow-slate-500/50 border-solid border-2 border-transparent mx-auto w-6/12 px-20 py-10 my-10 rounded-3xl ">
         <form>
-          <h2 className="mb-5 text-3xl">Sign up Form</h2>
+          <h2 className="mb-5 text-3xl font-bold ">Sign up Form</h2>
           <label className="block mb-3 text-xl" htmlFor="firstName">First Name <span className="text-orange">*</span></label>
           <input className="mb-5 border-solid border-2 border-lightGreen w-full h-11 pl-2" type="text" required />
           <label className="block mb-3 text-xl" htmlFor="lastName">Last Name <span className="text-orange">*</span></label>
@@ -21,8 +21,8 @@ export default function Newsletter() {
           <input className="mb-5 border-solid border-2 border-lightGreen w-full h-11 pl-2" type="email" required />
         </form>
         <p className="text-xl">Thank you</p>
-        <p className="text-right my-10"><span className="text-orange">*</span>Required Fields</p>
-        <button>Sign Up</button>
+        <p className="text-right my-10"><span className="text-xl text-orange">*</span>Required Fields</p>
+        <button className="hover:bg-lightGreen ml-auto bg-orange py-4 px-10 rounded-md font-bold text-xl block" >Sign Up</button>
       </div >
     </>
   )
