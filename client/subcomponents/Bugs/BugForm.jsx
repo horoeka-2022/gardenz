a613d064a00a77f09a425c022ae2a995d47fce46
daniel <dwalshjones@icloud.com>
2022-11-29 10:41:20 +1300

    make bugsHelper.js, edit form

--- client/subcomponents/Bugs/BugForm.jsx
+++ client/subcomponents/Bugs/BugForm.jsx
@@ -1,32 +1,27 @@
 import React from 'react'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
-import moment from 'moment'
+import { addBug } from './bugsHelper'
 
-const eventSchema = Yup.object({
-  title: Yup.string().required('Required'),
+const bugSchema = Yup.object({
+  title: Yup.string()
+    .max(30, 'Must be 30 characters or less')
+    .required('Required'),
   body: Yup.string().required('Required'),
 })
 
 export default function EventForm() {
-  const initialState = {
-    title: '',
-    date: '',
-    volunteersNeeded: 0,
-    description: '',
-  }
-  const event = initialState
-  const { title, body } = event
-
   const formik = useFormik({
     initialValues: {
-      title,
-      body,
+      title: '',
+      body: '',
     },
     onSubmit: (values) => {
-      props.submitEvent(values)
+      // console.log(values)
+      addBug(values)
+      formik.resetForm()
     },
-    validationSchema: eventSchema,
+    validationSchema: bugSchema,
   })
 
   function handleCancel(e) {
@@ -37,11 +32,11 @@ export default function EventForm() {
   return (
     <>
       <section>
-        <h2 className="form-title">{props.action}</h2>
+        <h2 className="form-title">Report Bug</h2>
         <form className="form-content" onSubmit={formik.handleSubmit}>
           <div className="field">
             <label htmlFor="title" className="label">
-              Event Title
+              Name your Bug
             </label>
             {formik.errors.title && formik.touched.title ? (
               <p className="inputError">{formik.errors.title}</p>
@@ -51,73 +46,31 @@ export default function EventForm() {
               id="title"
               name="title"
               type="text"
-              placeholder="event title"
               onChange={formik.handleChange}
               value={formik.values.title}
             />
 
-            <label htmlFor="date" className="label">
-              Date
+            <label htmlFor="body" className="label">
+              Describe your bug
             </label>
-            {formik.errors.date && formik.touched.date ? (
-              <p className="inputError">{formik.errors.date}</p>
-            ) : null}
-            <input
-              className="form-box"
-              id="date"
-              name="date"
-              type="date"
-              role="date"
-              placeholder="date"
-              onChange={formik.handleChange}
-              value={formik.values.date}
-            />
-
-            <label htmlFor="volunteersNeeded" className="label">
-              Volunteers Needed
-            </label>
-            {formik.errors.volunteersNeeded &&
-            formik.touched.volunteersNeeded ? (
-              <p className="inputError">{formik.errors.volunteersNeeded}</p>
-            ) : null}
-            <input
-              className="form-box"
-              id="volunteersNeeded"
-              name="volunteersNeeded"
-              type="number"
-              placeholder="volunteers needed"
-              min="0"
-              onChange={formik.handleChange}
-              value={formik.values.volunteersNeeded}
-            />
-
-            <label htmlFor="description" className="label">
-              Description
-            </label>
-            {formik.errors.description && formik.touched.description ? (
-              <p className="inputError">{formik.errors.description}</p>
+            {formik.errors.body && formik.touched.body ? (
+              <p className="inputError">{formik.errors.body}</p>
             ) : null}
             <textarea
-              className="description-box"
-              id="description"
-              name="description"
-              placeholder="event description"
+              id="body"
+              name="body"
               onChange={formik.handleChange}
-              value={formik.values.description}
+              value={formik.values.body}
             />
           </div>
 
-          <div className="button-group">
-            {props.action === 'Update Event' ? (
-              <button className="submit form-box" onClick={handleCancel}>
-                Cancel Event
-              </button>
-            ) : null}
+          <button className="submit form-box" type="submit">
+            Submit
+          </button>
 
-            <button className="submit form-box" type="submit">
-              Submit
-            </button>
-          </div>
+          <button className="submit form-box" onClick={handleCancel}>
+            Cancel
+          </button>
         </form>
       </section>
     </>
---
+++ client/subcomponents/Bugs/bugsHelper.js
@@ -0,0 +1,18 @@
+import requestor from '../../consume'
+import { dispatch } from '../../store'
+import { clearWaiting, setWaiting } from '../../slices/waiting'
+import { showError } from '../../slices/error'
+
+export function addBug(bugInput, consume = requestor) {
+  dispatch(setWaiting())
+  return consume('/bugs', null, 'post', { ...bugInput })
+    .then(() => {
+      dispatch(clearWaiting())
+    })
+    .catch((err) => {
+      dispatch(showError(err))
+    })
+    .finally(() => {
+      dispatch(clearWaiting())
+    })
+}
