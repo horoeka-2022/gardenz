16768f5525da6444ee6d46e4d0ec0a118b60860c
daniel <dwalshjones@icloud.com>
2022-11-28 16:21:12 +1300

    tidy up divs & redundant html elements

--- client/subcomponents/events/EventForm/EventForm.jsx
+++ client/subcomponents/events/EventForm/EventForm.jsx
@@ -39,108 +39,90 @@ export default function EventForm(props) {
   }
 
   return (
-    <>
-      <section className="mx-auto max-w-md mt-9 px-8">
-        <div className="">
-          <h2 className="form-title text-darkBlue text-[24px] font-serif py-3">
-            {props.action}
-          </h2>
-
-          <form
-            className="form-content text-[16px] text-black"
-            onSubmit={formik.handleSubmit}
-          >
-            <div className="field">
-              <label htmlFor="title" className="label">
-                Event Title
-              </label>
-              <div>
-                {formik.errors.title && formik.touched.title ? (
-                  <p className="inputError">{formik.errors.title}</p>
-                ) : null}
-                <input
-                  className="form-box border border-[1.5px] border-blue rounded-[4px] p-2 w-full mb-5"
-                  id="title"
-                  name="title"
-                  type="text"
-                  onChange={formik.handleChange}
-                  value={formik.values.title}
-                />
-              </div>
-              <label htmlFor="date" className="label">
-                Date
-              </label>
-              <div>
-                {formik.errors.date && formik.touched.date ? (
-                  <p className="inputError">{formik.errors.date}</p>
-                ) : null}
-                <input
-                  className="form-box border border-[1.5px] border-blue rounded-[4px] p-2 w-full mb-5"
-                  id="date"
-                  name="date"
-                  type="date"
-                  role="date"
-                  onChange={formik.handleChange}
-                  value={formik.values.date}
-                />
-              </div>
-              <label htmlFor="volunteersNeeded" className="label">
-                Volunteers Needed
-              </label>
-              <div>
-                {formik.errors.volunteersNeeded &&
-                formik.touched.volunteersNeeded ? (
-                  <p className="inputError">{formik.errors.volunteersNeeded}</p>
-                ) : null}
-                <input
-                  className="form-box border border-[1.5px] border-blue rounded-[4px] p-2 w-full mb-5"
-                  id="volunteersNeeded"
-                  name="volunteersNeeded"
-                  type="number"
-                  min="0"
-                  onChange={formik.handleChange}
-                  value={formik.values.volunteersNeeded}
-                />
-              </div>
-              <label htmlFor="description" className="label text-black">
-                Description
-              </label>
-              <div>
-                {formik.errors.description && formik.touched.description ? (
-                  <p className="inputError">{formik.errors.description}</p>
-                ) : null}
-                <textarea
-                  className="description-box border border-[1.5px] border-blue rounded-[4px] p-2 pb-[7em] w-full mb-3"
-                  id="description"
-                  name="description"
-                  onChange={formik.handleChange}
-                  value={formik.values.description}
-                />
-              </div>
-            </div>
-
-            <div className="button-group">
-              <div>
-                <button
-                  className="submit form-box h-7 w-48 bg-orange text-white font-bold rounded-[4px]"
-                  type="submit"
-                >
-                  Submit
-                </button>
-
-                {props.action === 'Update Event' ? (
-                  <button
-                    className="submit form-box h-7 w-48 font-bold rounded-[4px]"
-                    onClick={handleCancel}
-                  >
-                    Cancel Event
-                  </button>
-                ) : null}
-              </div>
-            </div>
-          </form>
+    <section className="mx-auto max-w-md mt-9 px-8">
+      <h2 className="form-title text-darkBlue text-[24px] font-serif py-3">
+        {props.action}
+      </h2>
+      <form
+        className="form-content text-[16px] text-black"
+        onSubmit={formik.handleSubmit}
+      >
+        <div className="field">
+          <label htmlFor="title" className="label">
+            Event Title
+          </label>
+          {formik.errors.title && formik.touched.title ? (
+            <p className="inputError">{formik.errors.title}</p>
+          ) : null}
+          <input
+            className="form-box border border-[1.5px] border-blue rounded-[4px] p-2 w-full mb-5"
+            id="title"
+            name="title"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.title}
+          />
+          <label htmlFor="date" className="label">
+            Date
+          </label>
+          {formik.errors.date && formik.touched.date ? (
+            <p className="inputError">{formik.errors.date}</p>
+          ) : null}
+          <input
+            className="form-box border border-[1.5px] border-blue rounded-[4px] p-2 w-full mb-5"
+            id="date"
+            name="date"
+            type="date"
+            role="date"
+            onChange={formik.handleChange}
+            value={formik.values.date}
+          />
+          <label htmlFor="volunteersNeeded" className="label">
+            Volunteers Needed
+          </label>
+          {formik.errors.volunteersNeeded && formik.touched.volunteersNeeded ? (
+            <p className="inputError">{formik.errors.volunteersNeeded}</p>
+          ) : null}
+          <input
+            className="form-box border border-[1.5px] border-blue rounded-[4px] p-2 w-full mb-5"
+            id="volunteersNeeded"
+            name="volunteersNeeded"
+            type="number"
+            min="0"
+            onChange={formik.handleChange}
+            value={formik.values.volunteersNeeded}
+          />
+          <label htmlFor="description" className="label text-black">
+            Description
+          </label>
+          {formik.errors.description && formik.touched.description ? (
+            <p className="inputError">{formik.errors.description}</p>
+          ) : null}
+          <textarea
+            className="description-box border border-[1.5px] border-blue rounded-[4px] p-2 pb-[7em] w-full mb-3"
+            id="description"
+            name="description"
+            onChange={formik.handleChange}
+            value={formik.values.description}
+          />
         </div>
-      </section>
-    </>
+        <div className="button-group">
+          <button
+            className="submit form-box h-7 w-48 bg-orange text-white font-bold rounded-[4px]"
+            type="submit"
+          >
+            Submit
+          </button>
+          {props.action === 'Update Event' ? (
+            <button
+              className="submit form-box h-7 w-48 font-bold rounded-[4px]"
+              onClick={handleCancel}
+            >
+              Cancel Event
+            </button>
+          ) : null}
+        </div>
+      </form>
+    </section>
   )
 }
